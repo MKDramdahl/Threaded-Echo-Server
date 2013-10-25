830b3364186284843adb28e072a9ca9b013833f5
mart2967 <mart2967@morris.umn.edu>
2013-10-25 12:47:14 -0500

    Server is working, client needs work

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,32 @@
 package echoserver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+
+
 public class EchoClient {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket((args[0]!=null)? args[0]:"localhost", 6013);
+
+            InputStream input = socket.getInputStream();
+            OutputStream output = socket.getOutputStream();
+            InputStreamReader reader = new InputStreamReader(input);
+
+            byte line;
+            while (true) {
+                output.write(input.read());
+            }
+
+            //socket.close();
+        } catch (IOException ioe) {
+            System.out.println("We caught an exception");
+            System.err.println(ioe);
+        }
+    }
+    
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,28 @@
 package echoserver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class EchoServer {
+    
+    public static void main(String[] args) {
+        try {
+            ServerSocket sock = new ServerSocket(6013);
+            
+            while (true) {
+                Socket client = sock.accept();
+                InputStreamReader isr = new InputStreamReader(client.getInputStream());
+                while (true) {
+                    byte input = (byte) isr.read();
+                    client.getOutputStream().write(input);
+                }
+            }
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        }
+    }
 }
