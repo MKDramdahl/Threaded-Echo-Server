55b561ab1eb36a5c14a93e985bc00d74271afc5e
mart2967 <mart2967@morris.umn.edu>
2013-11-04 21:27:14 -0600

    Added threading

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,31 +1,85 @@
 package echoserver;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
 
 public class EchoClient {
-    public static void main(String[] args) {
+
+    Socket socket;
+
+    public EchoClient() {
+        
+    }
+    
+    public static void main(String[] args) throws InterruptedException {
         try {
-            Socket socket = new Socket((args.length > 0) ? args[0]:"localhost", 6013);
-            int input;
-            OutputStream output = socket.getOutputStream();
+            EchoClient ec = new EchoClient();
+            ec.socket = new Socket((args.length > 0) ? args[0]:"localhost", 6013);
+
+            Thread localInput = new Thread(ec.new inputThread());
+            localInput.start();
+            Thread localOutput = new Thread(ec.new outputThread());
+            localOutput.start();
             
+            localInput.join();
+            localOutput.join();
             
-            while ((input = System.in.read()) != -1) {
-                //System.out.println("input: " + input);
-                output.write(input);
-                output.flush();
-                int outputVal = socket.getInputStream().read();
-                System.out.write(outputVal);
-                System.out.flush();
-            }
-            output.close();
-            socket.close();
+            ec.socket.close();
         } catch (IOException ioe) {
             System.out.println("We caught an exception");
-            System.err.println(ioe);
+            System.out.println(ioe);
         }
     }
+
+    private class outputThread implements Runnable {
+
+        public outputThread() {
+            
+        }
+        
+        @Override
+        public void run() {
+
+            int outputVal;
+            try {
+                InputStream input = socket.getInputStream();
+                while ((outputVal = input.read()) != -1) {
+                    System.out.write(outputVal);
+                }
+                System.out.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private class inputThread implements Runnable {
+
+        public inputThread(){
+        
+        }
+        
+        @Override
+        public void run() {
+            int input;
+            try {
+
+                OutputStream output = socket.getOutputStream();
+                while ((input = System.in.read()) != -1) {
+                    output.write(input);
+                }
+                output.flush();
+                socket.shutdownOutput();
+
+            } catch (Exception e) {
+                System.err.println(e);
+            }
+        }
+
+    }
+
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -7,36 +7,75 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class EchoServer {
-    
+public class EchoServer implements Runnable {
+    private final ServerSocket serverSocket;
+    private final ExecutorService pool;
+    protected int connectedClients = 0; 
+
+    public EchoServer(int port, int poolSize) throws IOException {
+        serverSocket = new ServerSocket(port);
+        pool = Executors.newFixedThreadPool(poolSize);
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        try {
+            for (;;) {
+                pool.execute(new Handler(serverSocket.accept()));
+            }
+        } catch (IOException ex) {
+            pool.shutdown();
+        }
+
+    }
+
     public static void main(String[] args) {
         try {
-            ServerSocket sock = new ServerSocket(6013);
-            
-            while (true) {
-                try {
-                Socket client = sock.accept();
-                InputStream isr = client.getInputStream();
-                OutputStream out = client.getOutputStream();
-                int input;
-                while ((input = isr.read()) != -1) {
-                    //System.out.println("Server input: " + input);
-                    
-                    out.write(input);
-                    out.flush();
-                    //System.out.println("Input written");
-                }
-                isr.close();
-                client.close();
-                }catch(SocketException e) {
-                    System.err.println(e);
-                    continue;
-                }
-            }
+            //ServerSocket sock = new ServerSocket(6013);
+            EchoServer server = new EchoServer(6013, 1000);
+            Thread serverThread = new Thread(server);
+            serverThread.start();
+
         } catch (IOException ioe) {
             System.err.println(ioe);
             ioe.printStackTrace();
         }
     }
+
+    class Handler implements Runnable {
+        private final Socket socket;
+        Handler(Socket socket) { this.socket = socket; }
+        @Override
+        public void run() {
+            connectedClients++;
+            System.out.println("Client connected! Clients: " + connectedClients);
+            try {
+                InputStream isr = socket.getInputStream();
+                OutputStream out = socket.getOutputStream();
+
+                int input;
+                while ((input = isr.read()) != -1) {
+                    //System.out.println("Server input: " + input);
+                    out.write(input);
+                    out.flush();
+                    //System.out.println("Input written");
+                }
+                isr.close();
+                socket.close();
+                connectedClients--;
+            }catch(Exception e) {
+                System.err.println(e);
+            }
+
+
+
+        }
+
+    }
+
+
 }
